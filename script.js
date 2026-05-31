592ca56b32eaa9b91010fa30682d2013900c8412
Marco Antonio <marcoantonionogaretti@gmail.com>
2026-05-31 11:32:47 -0300

    pusando posts para site

--- script.js
+++ script.js
@@ -1,11 +1,25 @@
 const urlAPI = "https://jsonplaceholder.typicode.com"
 
-buscarRota(urlAPI + "/posts");
-
 async function buscarRota(url){
     const res = await fetch(url);
-    if (res.status){
-        const obj = await res.json();
-        console.log(obj)
+    if (res.ok){
+        const obj = await res.json();        
+        return obj;
     }
 }
+
+async function main(){
+    const users = await buscarRota(urlAPI + "/users");
+    const posts = await buscarRota(urlAPI + "/posts");
+
+    console.log(posts.length);
+    for (let i = 0; i < posts.length; i++){
+        const post = posts[i];
+        document.getElementById("elemento").innerHTML += "<div class='card'>" + 
+            "<h2>" + post.title  + "</h2>" +
+            "<div>" + post.body + "</div>"
+            + "</div>";
+    }
+}
+
+main();

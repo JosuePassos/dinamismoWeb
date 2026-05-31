1294ef5dac1118c07253eaa69d14e215795404bf
Marco Antonio <marcoantonionogaretti@gmail.com>
2026-05-31 13:11:53 -0300

    adicionando username ao post

--- script.js
+++ script.js
@@ -12,11 +12,16 @@ async function main(){
     const users = await buscarRota(urlAPI + "/users");
     const posts = await buscarRota(urlAPI + "/posts");
 
-    console.log(posts.length);
+    console.log(users);
     for (let i = 0; i < posts.length; i++){
         const post = posts[i];
+        const userId = post["userId"];
+        const userCarac = users.find(u => u.id === userId);
+        const userName = userCarac["username"];
+
         document.getElementById("elemento").innerHTML += "<div class='card'>" + 
             "<h2>" + post.title  + "</h2>" +
+            "<h3>" + userName + "<h3>" +
             "<div>" + post.body + "</div>"
             + "</div>";
     }
